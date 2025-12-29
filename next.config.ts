4ddfa9878c82ab2e7259148c8596918602db3648
Kishan Asokan <kishanasokan@gmail.com>
2025-12-29 17:31:08 -0600

    image hosting

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,10 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'lh5.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.radiordle.org',
+      },
     ],
     unoptimized: false, // Enable Next.js image optimization for better performance
   },
